9b536ff3a2dd2d67fe11b4ecb0ec5853068f66b0
Avishek-Pratyay <avishekp.aiub@gmail.com>
2026-05-20 21:32:43 +0600

    backend update on render

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000",
+      "https://studynook-client-side.vercel.app"
+    ],
     credentials: true,
   })
 );
